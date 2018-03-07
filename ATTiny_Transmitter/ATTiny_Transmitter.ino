a849b7b6f2c0ed4fc7f755706857258dea244f58
Stephen McMahon <stephen@slmcmahon.com>
2018-03-06 21:05:11 -0600

    More refactoring

--- ATTiny_Transmitter/ATTiny_Transmitter.ino
+++ ATTiny_Transmitter/ATTiny_Transmitter.ino
@@ -3,6 +3,8 @@
 const int PIR_PIN = 1;
 const int TX_PIN = 2;
 const int TX_SPEED = 4000;
+const char *ON = "1";
+const char *OFF = "0";
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
@@ -13,7 +15,7 @@ void setup() {
 
 void loop() {
   int sensorValue = digitalRead(PIR_PIN);
-  char *message = sensorValue == 1 ? "1" : "0";
+  char *message = sensorValue == 1 ? ON : OFF;
 
   vw_send((uint8_t *)message, strlen(message));
   vw_wait_tx();
