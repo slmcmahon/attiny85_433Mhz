25313c7940280ba8974c9334a9c37a072ad3b1b3
Stephen McMahon <stephen@slmcmahon.com>
2018-03-06 20:44:41 -0600

    Refactored transmitter code.

--- ATTiny_Transmitter/ATTiny_Transmitter.ino
+++ ATTiny_Transmitter/ATTiny_Transmitter.ino
@@ -1,34 +1,22 @@
 #include <VirtualWire.h>
 
+const int PIR_PIN = 1;
+const int TX_PIN = 2;
 char *controller;
 
 void setup() {
-  pinMode(1, INPUT);
+  pinMode(PIR_PIN, INPUT);
   vw_set_ptt_inverted(true);
-  vw_set_tx_pin(2);
+  vw_set_tx_pin(TX_PIN);
   vw_setup(4000);
 }
 
-
 void loop() {
-  
-  int sensorValue = digitalRead(1);
+  int sensorValue = digitalRead(PIR_PIN);
+  char *message = sensorValue == 1 ? "1" : "0";
 
-  if(sensorValue == 1)
-  {
-    sendMessage("1");
-  }
-
-  else
-  {
-    sendMessage("0");
-  }
-  
-  delay(1);        // delay in between reads for stability
-}
-
-void sendMessage(char *message) {
   vw_send((uint8_t *)message, strlen(message));
   vw_wait_tx();
-}
 
+  delay(1);        // delay in between reads for stability
+}
