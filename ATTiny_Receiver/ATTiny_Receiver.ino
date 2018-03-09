eacba5c3eff86f2ccf30dc8772b48a5bdf817ae7
Stephen McMahon <stephen@slmcmahon.com>
2018-03-08 19:32:06 -0600

    Some minor cleanup.  Added gitignore.

--- ATTiny_Receiver/ATTiny_Receiver.ino
+++ ATTiny_Receiver/ATTiny_Receiver.ino
@@ -3,6 +3,7 @@
 const int LED_PIN = 0;
 const int RCV_PIN = 1;
 const int TX_SPEED = 4000;
+const char *ON = "1";
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -16,7 +17,7 @@ void loop() {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   if (vw_get_message(buf, &buflen)) {
-     int val = buf[0] == '1' ? HIGH : LOW;
+     int val = buf[0] == ON ? HIGH : LOW;
      digitalWrite(LED_PIN, val);
   }
 }
